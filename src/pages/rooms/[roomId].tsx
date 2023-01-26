bf7d7b625f7dd9855e2bc4e455b7116a9c1bdc08
Rainer Piil <piilrainer@gmail.com>
2023-01-26 22:56:22 +0200

    changed chat chip and added basic message socket conf

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,35 +1,28 @@
 import { ActionIcon, Button, Center, CopyButton, Drawer, Flex, Grid, Paper, Tooltip, Transition } from '@mantine/core';
-import { AspectRatio, MediaQuery, TextInput } from '@mantine/core';
 import { useRouter } from 'next/router'
-import { NextPage } from 'next/types';
+import type { NextPage } from 'next/types';
 import { useEffect, useState } from 'react';
 import Chat from '../../components/room/Chat';
-import { SendMessage, SendMessageTest, Color } from '../../constants/schema';
-import { api } from '../../utils/api';
-import { IconCheck, IconLink, IconMessage, IconMessageCircle, IconSettings } from '@tabler/icons'
+import type { SendMessage, SendMessageTest } from '../../constants/schema';
+// import { api } from '../../utils/api';
+import { IconCheck, IconLink, IconMessageCircle, IconSettings } from '@tabler/icons'
 import { slideLeft } from '../../styles/transitions';
-import io, { Socket, SocketOptions } from 'socket.io-client'
-import { DefaultEventsMap } from '@socket.io/component-emitter';
+import type { Socket } from 'socket.io-client';
+import io from 'socket.io-client'
+import type { DefaultEventsMap } from '@socket.io/component-emitter';
 import { Events } from '../../constants/events';
 let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
-
-type RoomConfig =
-    {
-        bgColor?: Color;
-    }
 
 const Room: NextPage = () => {
     const router = useRouter();
     const roomId = router.query.roomId as string;
-    const [messages, setMessages] = useState<[SendMessageTest]>();
-    const [message, setMessage] = useState<SendMessage>()
-    const [test, setTest] = useState<string>("")
+    // const [messages, setMessages] = useState<[SendMessageTest]>();
     const [chatOpen, setChatOpen] = useState<string>("flex");
-    const [bgColor, setBgColor] = useState<Color>("#000000");
+    // const [bgColor, setBgColor] = useState<Color>("#000000");
     const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
 
     useEffect(() => {
-        socketInitializer()
+        void socketInitializer()
     }, [])
 
 
@@ -41,16 +34,15 @@ const Room: NextPage = () => {
             console.log('connected')
         })
 
-        socket.on(Events.ON_SOCKET_TEST, msg => {
-            var test: SendMessage = msg;
-            setMessage(msg);
+        socket.on(Events.SEND_MESSAGE_UPDATE, msg => {
+            console.log(msg);
         })
     }
 
-    const onSocketTest = () => {
-        const test2: SendMessage = { message: test, roomId: roomId }
-        socket.emit(Events.SOCKET_TEST, test2)
+    const sendMessageWs = (message: SendMessageTest) => {
+        socket.emit(Events.SEND_MESSAGE, message)
     }
+
     //#region TRPC queries
     //#endregion
 
@@ -61,9 +53,6 @@ const Room: NextPage = () => {
 
     return (
         <>
-            <code> {JSON.stringify(message)}</code>
-            <input value={test} onChange={e => setTest(e.target.value)}></input>
-            <Button onClick={onSocketTest}>Test</Button>
             <Drawer
                 position='top'
                 opened={settingsOpen}
@@ -75,14 +64,14 @@ const Room: NextPage = () => {
                 <h4>Conetn in here</h4>
             </Drawer>
 
-            <Flex style={{ backgroundColor: bgColor, width: "100%", height: "100%" }} direction="row" justify={"flex-end"}>
+            <Flex style={{ backgroundColor: "black", width: "100%", height: "100%" }} direction="row" justify={"flex-end"}>
                 <Center style={{ width: "100%", height: "100%" }}>
                     <video src='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4' width="100%" height="100%" controls />
                 </Center>
 
                 {chatOpen != "flex" ?
 
-                    //Maybe make function that adds buttons based on json
+                    //Maybe make function that adds buttons based on json (different component)
                     <Button.Group orientation="vertical" style={{ position: "absolute", right: 40, top: 40, gap: 2 }}>
 
                         <Tooltip position='left' label="Open chat">
@@ -113,23 +102,10 @@ const Room: NextPage = () => {
                     : null}
                 <Transition mounted={chatOpen === "flex"} transition={slideLeft} duration={200} timingFunction="ease">
                     {(styles) => (
-                        <Chat styles={styles} chatOpen={chatOpen} setChatOpen={setChatOpen} messages={[{ message: "test", user: "Rainer" }]} />
+                        <Chat styles={styles} chatOpen={chatOpen} sendMessageWs={sendMessageWs} setChatOpen={setChatOpen} messages={[{ message: "test", user: "Rainer" }]} />
                     )}
                 </Transition>
             </Flex >
-            {/* // <Grid m={1} columns={18} style={{ border: "solid yellow 1px " }}>
-        //     <Grid.Col style={{ alignItems: "center", border: "solid red 1px", height: "100%" }}>
-        //         <AspectRatio ratio={16 / 9}>
-        //             <video controls />
-        //         </AspectRatio>
-        //     </Grid.Col>
-
-        //     <Grid.Col pos="fixed" style={{ right: "0", width: "100%", height: "100%" }} span={4}>
-        //
-        //     </Grid.Col>
-
-        // </Grid>
-        </> */}
         </>
     );
 }
