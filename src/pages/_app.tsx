1a8d2accec32718d797c1566792fcc12c2cd3557
Rainer Piil <piilrainer@gmail.com>
2023-02-12 22:35:54 +0200

    temp nav hide

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,8 @@ import NavFooter from "../components/navigation/NavFooter";
 import NavSideBar from "../components/navigation/NavSideBar";
 import AppSideNav from "../components/navigation/AppSideNav";
 import NavHeader from "../components/navigation/NavHeader";
+import { useEffect } from "react"
+import { useRouter } from "next/router";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -20,8 +22,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({ key: "color-scheme", defaultValue: "light" });
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-
   const [opened, setOpened] = useState(false);
+  const [hideNav, setHideNav] = useState(false);
+  const router = useRouter();
+
+  //definitely temporary
+  useEffect(() => {
+    if (router.pathname === "/signup" || router.pathname === "/") {
+      setHideNav(true)
+    }
+    console.log(router.pathname)
+  }, [])
+
+
   return (
 
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
@@ -30,7 +43,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
           <SessionProvider session={session}>
             <>
               {/* Find A system to turn navbar off and on in every page  */}
-              <AppShell style={{ height: "100%" }}
+              <AppShell hidden={hideNav} style={{ height: "100%" }}
                 navbarOffsetBreakpoint="sm"
                 asideOffsetBreakpoint="sm"
                 navbar={
