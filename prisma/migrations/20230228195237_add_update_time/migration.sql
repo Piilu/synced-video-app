cf42b1b9971bd67cd34691d9710663191a9ae3d1
Rainer Piil <piilrainer@gmail.com>
2023-02-28 22:10:06 +0200

    fixes nextjs dev parse

---
+++ prisma/migrations/20230228195237_add_update_time/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Example` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updatedAt` to the `Room` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Video` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Room` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Video` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- DropTable
+DROP TABLE `Example`;
